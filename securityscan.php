b54a418e33f6f079fd8bd84db46a62450b07fdd2
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-03 05:48:01 +0000

    UPDATE filesb
    git-svn-id: https://plugins.svn.wordpress.org/wp-security-scan/trunk@37936 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- securityscan.php
+++ securityscan.php
@@ -4,7 +4,7 @@ Plugin Name: WP Security Scan
 Plugin URI: http://wordpress.org/extend/plugins/wp-security-scan/
 Description: Perform security scan of WordPress installation.
 Author: Michael Torbert
-Version: 2.2.2
+Version: 2.2.3
 Author URI: http://semperfiwebdesign.com/
 */
 require_once('../wp-content/plugins/securityscan/menu.php');
