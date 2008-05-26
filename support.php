d6096058a4b9ba64211863e769b5949546d15a34
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-26 01:56:40 +0000

    man table prefix
    git-svn-id: https://plugins.svn.wordpress.org/wp-security-scan/trunk@47837 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- support.php
+++ support.php
@@ -9,6 +9,8 @@ Under Construction...<br /><br />
 <ul>
 <li><a href='http://semperfiwebdesign.com/documentation/wp-security-scan/changelog/' target="_blank">Changelog</a></li>
 <li><a href='http://semperfiwebdesign.com/documentation/wp-security-scan/' target="_blank">Documentation</a></li>
+<li><a href='http://semperfiwebdesign.com/documentation/wp-security-scan/change-wordpress-database-table-name-prefix/' target="_blank">Change WordPress Databse Table Name Prefix</a></li>
+
 </ul>
 <br /><br /><strong>Backup early, backup often!</strong><br /><br /><br /><br /><br />
               <em>For comments, suggestions, bug reporting, etc please <a href="http://semperfiwebdesign.com/contact/">click here</a>.</em>
