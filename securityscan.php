3b211081ebe981fb750b6ec80ef8661506f17891
hallsofmontezuma <hallsofmontezuma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-08 00:49:29 +0000

    tagging version 2.3.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-security-scan/tags/2.3.3@100303 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- securityscan.php
+++ securityscan.php
@@ -4,12 +4,12 @@ Plugin Name: WP Security Scan
 Plugin URI: http://semperfiwebdesign.com/plugins/wp-security-scan/
 Description: Perform security scan of WordPress installation.
 Author: Michael Torbert
-Version: 2.3
+Version: 2.3.3
 Author URI: http://semperfiwebdesign.com/
 */
 
 /*
-Copyright (C) 2008 semperfiwebdesign.com (michael AT semperfiwebdesign DOT com)
+Copyright (C) 2008-2009 Michael Torbert / semperfiwebdesign.com (michael AT semperfiwebdesign DOT com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -68,110 +68,137 @@ if($mrt_latest > $mrt_version)
       echo "Latest Version";
   */  }
 
+
+	function yourplugin_helloworld_meta_box(){  
+	
+		global $wpdb;
+		mrt_check_version();
+		mrt_check_table_prefix();
+		mrt_version_removal();
+		mrt_errorsoff();
+		echo '<div style="color:green">WP ID META tag removed form WordPress core</div>';
+
+		$name = $wpdb->get_var("SELECT user_login FROM $wpdb->users WHERE user_login='admin'");
+		if ($name=="admin"){
+		  echo '<a href="http://semperfiwebdesign.com/documentation/wp-security-scan/change-wordpress-admin-username/" title="WordPress Admin" target="_blank"><font color="red">"admin" user exists.</font></a>';
+		  }
+		  else{
+		      echo '<font color="green">No user "admin".</font>';
+		      }
+		?><br /><?php 
+		$filename = '.htaccess';
+		if (file_exists($filename)) {
+		    echo '<font color="green">.htaccess exists in wp-admin/</font>';
+		} else {
+		    echo '<font color="red">The file .htaccess does not exist in wp-admin/.</font>';
+		}
+
+		?>
+
+		<div style="text-align:center;color:grey;margin-top:20px;"><em>**WP Security Scan plugin must remain active for security features to remain**</em></div>
+		
+		<div style="text-align:center;font-weight:bold;">Future Releases</div>
+		<ul><li>one-click change file/folder permissions</li><li>test for XSS vulnerabilities</li><li>intrusion detection/prevention</li><li>lock out/log incorrect login attempts</li><li>user enumeration protection</li><li>WordPress admin protection/security</li></ul>
+		<?php	}
+		
+		
+	function yourplugin_helloworld_meta_box2(){ ?>
+		<div style="padding-left:10px;">
+			<?php mrt_get_serverinfo(); ?>
+		</div>
+			
+		<?php	}
+	
+	
+	function yourplugin_helloworld_meta_box3(){  
+		
+		?>
+		
+		<div style="text-align:center"><em>This plugin is updated as a free service to the WordPress community.  Donations of any size are appreciated.</em>
+		<br /><br />
+		<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mrtorbert%40gmail%2ecom&item_name=Support%20WordPress%20Security%20Scan%20Plugin&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8" target="_blank">Click here to support this plugin.</a>
+		<br /><br /><h4>Highest Donations</h4></div><?php 
+
+		/*$ch = curl_init("http://semperfiwebdesign.com/top_donations.php");
+		$fp = fopen("top_donations.php", "w");
+		curl_setopt($ch, CURLOPT_FILE, $fp);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_exec($ch);
+		curl_close($ch);
+		fclose($fp);
+		*/
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://semperfiwebdesign.com/top_donations.php");
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_exec($ch);
+		curl_close($ch);
+
+		?>
+		<br /><br /><div style="text-align:center"><h4>Recent Donations</h4></div><?php
+
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://semperfiwebdesign.com/recent_donations.php");
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_exec($ch);
+		curl_close($ch);
+
+		/*
+		$ch = curl_init("http://semperfiwebdesign.com/recent_donations.php");
+		$fp = fopen("recent_donations.php", "w");
+		curl_setopt($ch, CURLOPT_FILE, $fp);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_exec($ch);
+		curl_close($ch);
+		fclose($fp);
+		*/
+		
+		}
+	
+
 function mrt_opt_mng_pg() {
         ?>
+
+
+<div class="wrap">
+    <h2>WP - Security Admin Tools</h2>  
+
 <!--<div id='update-nag'>A new version of WP Security Scan is available!</div>-->
 <?php //$rss = fetch_rss('http://alexrabe.boelinger.com/?tag=nextgen-gallery&feed=rss2');?>
 
-<div class=wrap>
-                <h2><?php _e('WP - Security Admin Tools') ?></h2>
-<br /><em>For comments, suggestions, bug reporting, etc please <a href="http://semperfiwebdesign.com/contact/">click here</a>.</em>
 
-          <div>
-<!--               <div id="message" class="updated fade"><p></p></div>-->
-<br /><div style="float: left;width: 33%; height: 100%;border: 1px solid #999;margin: 0 15px 15px 0;padding: 5px;">
-<div style="text-align:center;font-weight:bold;"><h3>Initial Scan</h3></div>
-<?php
+<?php  
 
-///////////
-
-///////////////////
-
-global $wpdb;
-mrt_check_version();
-mrt_check_table_prefix();
-mrt_version_removal();
-mrt_errorsoff();
-echo '<div style="color:green">WP ID META tag removed form WordPress core</div>';
-
-$name = $wpdb->get_var("SELECT user_login FROM $wpdb->users WHERE user_login='admin'");
-if ($name=="admin"){
-  echo '<a href="http://semperfiwebdesign.com/documentation/wp-security-scan/change-wordpress-admin-username/" title="WordPress Admin" target="_blank"><font color="red">"admin" user exists.</font></a>';
-  }
-  else{
-      echo '<font color="green">No user "admin".</font>';
-      }
-?><br /><?php 
-$filename = '.htaccess';
-if (file_exists($filename)) {
-    echo '<font color="green">.htaccess exists in wp-admin/</font>';
-} else {
-    echo '<font color="red">The file .htaccess does not exist in wp-admin/.</font>';
-}
+add_meta_box("yourplugin_helloworld", 'Initial Scan', "yourplugin_helloworld_meta_box", "wpss");  
+add_meta_box("yourplugin_helloworld", 'System Information Scan', "yourplugin_helloworld_meta_box2", "wpss2");  
+add_meta_box("yourplugin_helloworld", 'Donations', "yourplugin_helloworld_meta_box3", "wpss3");  
 
 ?>
 
-<br /><br /><br />
-<div style="text-align:center;color:grey;"><em>**click on an above link for documentation**</em></div>
-<br />
-<hr align=center size=2 width=33%>
-<br />
+<div id="dashboard-widgets-wrap">
+<div class="metabox-holder">
+	<div style="float:left; width:48%;" class="inner-sidebar">
+		<?php do_meta_boxes('wpss','advanced','');  ?>	
+	</div>
 
-<div style="text-align:center;font-weight:bold;"><h3>Future Releases</h3></div>
-<ul><li>one-click change file/folder permissions</li><li>test for XSS vulnerabilities</li><li>intrusion detection/prevention</li><li>lock out/log incorrect login attempts</li><li>user enumeration protection</li><li>WordPress admin protection/security</li></ul>
-</div>
-
-
-<div style="float: left; height: 30%;border: 1px solid #999;margin: 0 15px 15px 0;padding: 15px;">
-<div style="text-align:center;font-weight:bold;"><h3>System Information Scan</h3></div>
-<?php mrt_get_serverinfo(); ?>
-</div>
-
-<div style="float: left;width: 30%; border: 1px solid #999;">
-<div style="text-align:center;font-weight:bold;"><h3>Donations</h3></div>
-<div style="text-align:center"><em>This plugin is updated as a free service to the WordPress community.  Donations of any size are appreciated.</em>
-<br /><br />
-<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=mrtorbert%40gmail%2ecom&item_name=Support%20WordPress%20Security%20Scan%20Plugin&no_shipping=0&no_note=1&tax=0&currency_code=USD&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8" target="_blank">Click here to support this plugin.</a>
-<br /><br /><h4>Highest Donations</h4></div><?php 
-
-/*$ch = curl_init("http://semperfiwebdesign.com/top_donations.php");
-$fp = fopen("top_donations.php", "w");
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
-*/
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://semperfiwebdesign.com/top_donations.php");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-curl_close($ch);
-
-?>
-<br /><br /><div style="text-align:center"><h4>Recent Donations</h4></div><?php
-
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://semperfiwebdesign.com/recent_donations.php");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-curl_close($ch);
-
-/*
-$ch = curl_init("http://semperfiwebdesign.com/recent_donations.php");
-$fp = fopen("recent_donations.php", "w");
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-curl_close($ch);
-fclose($fp);
-*/
-?>
-</div>
+	<div style="float:right; width:48%; " class="inner-sidebar">
+		<?php do_meta_boxes('wpss2','advanced',''); ?>	
+	</div>
+						
 <div style="clear:both"></div>
+						
+	<div style="float:left;width:500px;" class="inner-sidebar">
+		<?php do_meta_boxes('wpss3','advanced','');  ?>	
+	</div>
 </div>
+
+<div style="clear:both;"></div>
+</div>
+	
+	<br /><em>For comments, suggestions, bug reporting, etc please <a href="http://semperfiwebdesign.com/contact/">click here</a>.</em>
+
+       
              Plugin by <a href="http://semperfiwebdesign.com/" title="Semper Fi Web Design">Semper Fi Web Design</a>
         </div>
 <?php } 
